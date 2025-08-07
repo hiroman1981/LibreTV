1d14eb68e1bc20f6d7c21fba68ae5c7ce04be5e4
hiroman1981 <hiroman1981@yandex.ru>
2025-08-08 07:13:01 +0800

    更新 customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    wwzy: {
+        api: 'https://wwzy.tv/api.php/provide/vod',
+        name: '旺旺短剧'
     }
 };
 
