5d74fdd28b01601d832e4635eaa9790f10e03d38
hiroman1981 <hiroman1981@yandex.ru>
2025-08-10 09:34:17 +0800

    更新 app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["mozhua","tyyszy","dyttzy", "bfzy","ruyi"]'); // 默认选中资源
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["ruyi","mozhua","tyyszy","dyttzy", "bfzy"]'); // 默认选中资源
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 默认选中资源
-        selectedAPIs = ["mozhua","tyyszy", "bfzy", "dyttzy","ruyi"];
+        selectedAPIs = ["ruyi","mozhua","tyyszy", "bfzy", "dyttzy"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
 
         // 默认选中过滤开关
